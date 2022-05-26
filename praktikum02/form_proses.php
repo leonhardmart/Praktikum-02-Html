a542482428b3659bd5910ae0ebc0b61bd39d8679
leonhardmart <100025495+leonhardmart@users.noreply.github.com>
2022-05-26 21:48:23 +0700

    Add files via upload

---
+++ praktikum02/form_proses.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Process</title>
+</head>
+<body>
+    <h1>Form Submission with GET Method</h1>
+    <p>Welcome <?php echo $_GET["username"];?></p>
+    <p>Your password is <?php echo $_GET["remember"];?></P>
+    <P>Remember me is <?php echo $_GET["remember"];?></p>
+    <p><a href="form_login7.html">Back to previous page</a></p>
+</body>
+</html>
